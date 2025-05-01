94e199de94ad4f33cc5826f3b1d6eb42bef2a625
potatoed6 <136747954+potatoed6@users.noreply.github.com>
2025-05-01 22:43:35 +0200

    Update LizardAccentSystem.cs

--- Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/LizardAccentSystem.cs
@@ -5,11 +5,15 @@ namespace Content.Server.Speech.EntitySystems;
 
 public sealed class LizardAccentSystem : EntitySystem
 {
-    private static readonly Regex RegexLowerS = new("s+");
-    private static readonly Regex RegexUpperS = new("S+");
-    private static readonly Regex RegexInternalX = new(@"(\w)x");
-    private static readonly Regex RegexLowerEndX = new(@"\bx([\-|r|R]|\b)");
-    private static readonly Regex RegexUpperEndX = new(@"\bX([\-|r|R]|\b)");
+    private static readonly Regex RegexLowerS = new("с+");
+    private static readonly Regex RegexUpperS = new("С+");
+    private static readonly Regex RegexLowerSh = new("ш+");
+    private static readonly Regex RegexUpperSh = new("Ш+");
+    private static readonly Regex RegexLowerShch = new("щ+");
+    private static readonly Regex RegexUpperShch = new("Щ+");
+    private static readonly Regex RegexInternalX = new(@"(\w)кс");
+    private static readonly Regex RegexLowerEndX = new(@"\bкс([\-|r|R]|\b)");
+    private static readonly Regex RegexUpperEndX = new(@"\bКС([\-|r|R]|\b)");
 
     public override void Initialize()
     {
@@ -22,15 +26,23 @@ public sealed class LizardAccentSystem : EntitySystem
         var message = args.Message;
 
         // hissss
-        message = RegexLowerS.Replace(message, "sss");
+        message = RegexLowerS.Replace(message, "ссс");
         // hiSSS
-        message = RegexUpperS.Replace(message, "SSS");
+        message = RegexUpperS.Replace(message, "ССС");
+        // lower SH
+        message = RegexLowerSh.Replace(message, "шшш");
+        // upper SH
+        message = RegexUpperSh.Replace(message, "ШШШ");
+        // lower SHCH
+        message = RegexLowerShch.Replace(message, "щщщ");
+        // upper SHCH
+        message = RegexUpperShch.Replace(message, "ЩЩЩ");
         // ekssit
-        message = RegexInternalX.Replace(message, "$1kss");
+        message = RegexInternalX.Replace(message, "$1ксс");
         // ecks
-        message = RegexLowerEndX.Replace(message, "ecks$1");
+        message = RegexLowerEndX.Replace(message, "ексс$1");
         // eckS
-        message = RegexUpperEndX.Replace(message, "ECKS$1");
+        message = RegexUpperEndX.Replace(message, "ЕКСС$1");
 
         args.Message = message;
     }
