48d74923e4c4b013c05a049b7b4b5846fc1f2fbd
potatoed6 <136747954+potatoed6@users.noreply.github.com>
2025-05-02 23:31:05 +0200

    Update MothAccentSystem.cs

--- Content.Server/Speech/EntitySystems/MothAccentSystem.cs
+++ Content.Server/Speech/EntitySystems/MothAccentSystem.cs
@@ -5,9 +5,10 @@ namespace Content.Server.Speech.EntitySystems;
 
 public sealed class MothAccentSystem : EntitySystem
 {
-    private static readonly Regex RegexLowerBuzz = new Regex("z{1,3}");
-    private static readonly Regex RegexUpperBuzz = new Regex("Z{1,3}");
-
+    private static readonly Regex RegexLowerBuzz = new Regex("з{1,3}");
+    private static readonly Regex RegexUpperBuzz = new Regex("З{1,3}");
+    private static readonly Regex RegexLowerBuzh = new Regex("ж{1,3}");
+    private static readonly Regex RegexUpperBuzh = new Regex("Ж{1,3}");
     public override void Initialize()
     {
         base.Initialize();
@@ -19,9 +20,13 @@ public sealed class MothAccentSystem : EntitySystem
         var message = args.Message;
 
         // buzzz
-        message = RegexLowerBuzz.Replace(message, "zzz");
+        message = RegexLowerBuzz.Replace(message, "ззз");
         // buZZZ
-        message = RegexUpperBuzz.Replace(message, "ZZZ");
+        message = RegexUpperBuzz.Replace(message, "ЗЗЗ");
+        // buzzh
+        message = RegexLowerBuzh.Replace(message, "жжж");
+        // buZZH
+        message = RegexUpperBuzh.Replace(message, "ЖЖЖ");
 
         args.Message = message;
     }
